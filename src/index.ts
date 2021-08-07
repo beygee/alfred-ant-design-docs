7e175984bc2ea1650f7f5317bf8d3fbc7aa1736d
ByeongGyu Kim <doug0476@naver.com>
2021-08-07 16:39:24 +0900

    feat: add cache

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import algoliasearch from 'algoliasearch'
-import alfy from 'alfy'
+import alfy, { ScriptFilterItem } from 'alfy'
+const alfredNotifier = require('alfred-notifier')
+
+alfredNotifier()
 
 const client = algoliasearch('BH4D9OD16A', '60ac2c1a7d26ab713757e4a081e133d0')
 const index = client.initIndex('ant_design')
@@ -20,15 +23,32 @@ interface AlgoliaHitObjectHierarchy {
   lvl6: string | null
 }
 
-const start = async () => {
-  const { hits } = await index.search<AlgoliaHitObject>('icon', {
+const fetchWithCache = async (searchText: string) => {
+  const cachedItems = alfy.cache.get<string, ScriptFilterItem[]>(searchText)
+  if (cachedItems) return cachedItems
+
+  const { hits } = await index.search<AlgoliaHitObject>(searchText, {
     hitsPerPage: 20,
     facetFilters: ['tags:en'],
   })
 
   const filteredHits = hits.filter((hit) => hit.anchor === 'header')
 
-  alfy.output(filteredHits.map((hit) => ({ title: hit.hierarchy.lvl1 ?? '' })))
+  const items = filteredHits.map<ScriptFilterItem>((hit) => ({
+    title: hit.hierarchy.lvl1 ?? '',
+    subtitle: hit.hierarchy.lvl0 ?? '',
+    arg: hit.url,
+  }))
+
+  alfy.cache.set(searchText, items, { maxAge: 1000 * 60 * 60 })
+
+  return items
+}
+
+const start = async () => {
+  const searchText = alfy.input
+  const items = await fetchWithCache(searchText)
+  alfy.output(items)
 }
 
 start()
